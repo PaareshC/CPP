d0b150f7167990f0d0d2d93cb9f87b908f056c32
PaareshC <paareshchaudhari@gmail.com>
2020-08-07 18:40:41 +0530

    Size of data types done

---
+++ 3_Size_of_data_types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	const double PI=3.1415926535;
+	char myGrade='A';
+	bool isHappy=true;
+	int myAge=39;
+	float favNum=3.131492;
+	double otherfavNum=1.6180339887;
+	cout<<"Size of double is "<<sizeof(favNum)<<endl;
+	cout<<"Size of char is "<<sizeof(myGrade) <<endl;
+	cout<< "Size of bool is "<<sizeof(isHappy)<<endl;
+	cout<<"Size of integer is "<<sizeof(myAge)<<endl;
+	return 0;
+}
