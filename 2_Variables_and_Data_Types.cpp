b04843e605282e202346255918b2068f59e08b68
PaareshC <paareshchaudhari@gmail.com>
2020-08-07 18:32:04 +0530

    Variables and Data Type Done

---
+++ 2_Variables_and_Data_Types.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	const double PI=3.1415926535;
+	char myGrade='A';
+	bool isHappy=true;
+	int myAge=39;
+	float favNum=3.131492;
+	double otherfavNum=1.6180339887;
+	cout<<"Favorite Number "<<favNum<<endl;
+	cout<<"Value of PI is "<<PI <<endl;
+	cout<<"My Grade is "<<myGrade <<endl;
+	cout<< "I am number "<<isHappy<<endl;
+	cout<<"I am "<<myAge<<" Years  Old" <<endl;
+	return 0;
+}
